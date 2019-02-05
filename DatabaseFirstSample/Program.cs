26d6a04daffcae3bc9bd36d7da46ce4252816404
Dillorom <37942024+Dillorom@users.noreply.github.com>
2019-02-05 13:55:22 -0800

    Add project files.

---
+++ DatabaseFirstSample/Program.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirstSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                Console.WriteLine("Enter a name to a new blog: ");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
